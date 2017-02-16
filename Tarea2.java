86f51330bb9ce4498696d5ee14a2fce7e1a60a9d
Alejandro Hurtado <alejogtunit460@gmail.com>
2017-02-16 10:07:25 -0600

    Commit Inicial

---
+++ Tarea2.java
@@ -0,0 +1,12 @@
+package tarea2;
+import java.io.Console;
+
+public class Tarea2 {
+
+    public static void main(String[] args) {
+        int numero = 10;
+        System.out.println(numero++);
+        numero = 10;
+        System.out.println(++numero);
+    }
+}
